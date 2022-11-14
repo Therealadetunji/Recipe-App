b6949ac3b0829e26a80593fd0bc87ab8d8082c62
gak92 <44802922+gak92@users.noreply.github.com>
2022-11-15 02:31:18 +0500

    Update actions to show food list, create new food, delete food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,9 +1,35 @@
 class FoodsController < ApplicationController
-  def index; end
+  def index;
+    # @foods = Food.all
+    @foods = current_user.foods
+    @cuser = current_user
+  end
 
   def show; end
 
-  def new; end
+  def new; 
+    @food = Food.new
+  end
 
-  def create; end
+  def create; 
+    @food = current_user.foods.new(food_data)
+
+    if @food.save
+      redirect_to foods_path
+    else
+      render :new, status: 'Error occured with Food!'
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path
+  end
+
+  private
+
+  def food_data
+    params.require(:foods).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
