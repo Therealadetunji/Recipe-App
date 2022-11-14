a3d42d08b125523908bea03fdbb44e0747d62f5b
gak92 <44802922+gak92@users.noreply.github.com>
2022-11-15 02:31:59 +0500

    Add more entries to create for food table

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(name: 'Ali')
-User.create(name: 'Michael')
-User.create(name: 'Test')
+# User.create(name: 'Ali')
+# User.create(name: 'Michael')
+# User.create(name: 'Test')
+
+Food.create(name: "Apple", measurement_unit: "grams", price: 5.0, quantity: 1, user_id: 1)
+Food.create(name: "Pineapple", measurement_unit: "grams", price: 1.0, quantity: 2, user_id: 1)
+Food.create(name: "Chicken Breasts", measurement_unit: "units", price: 2.0, quantity: 5, user_id: 2)
