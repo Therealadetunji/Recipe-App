3ac206e89206b3ef7ecbd24153c9f0613f21da7c
gak92 <44802922+gak92@users.noreply.github.com>
2022-11-16 03:21:35 +0500

    Update schema with change column name in RecipeFood

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_132911) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_212324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,13 +26,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_132911) do
   end
 
   create_table "recipe_foods", force: :cascade do |t|
-    t.bigint "recipes_id", null: false
-    t.bigint "foods_id", null: false
+    t.bigint "recipe_id", null: false
+    t.bigint "food_id", null: false
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["foods_id"], name: "index_recipe_foods_on_foods_id"
-    t.index ["recipes_id"], name: "index_recipe_foods_on_recipes_id"
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -54,7 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_132911) do
   end
 
   add_foreign_key "foods", "users"
-  add_foreign_key "recipe_foods", "foods", column: "foods_id"
-  add_foreign_key "recipe_foods", "recipes", column: "recipes_id"
+  add_foreign_key "recipe_foods", "foods"
+  add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
 end
