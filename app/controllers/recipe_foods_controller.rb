727833c901364015d5b7c9640bef0685bc712901
gak92 <44802922+gak92@users.noreply.github.com>
2022-11-16 03:22:42 +0500

    Add create and destroy functionality

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,4 +1,31 @@
 class RecipeFoodsController < ApplicationController
-  def index
+  
+  def new
+    @recipe_food = RecipeFood.new
+    @recipe = Recipe.find(params[:recipe_id])
+    @foods = current_user.foods
+  end
+
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = @recipe.recipe_foods.new(recipe_food_params)
+
+    if @recipe_food.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new, status: 'Error occured with Recipe Food!'
+    end
+  end
+
+  def destroy
+    @recipe_food = RecipeFood.find(params[:id])
+    @recipe_food.destroy
+    redirect_to recipe_path(params[:recipe_id])
+  end
+
+  private
+
+  def recipe_food_params
+    params.require(:recipe_food).permit(:quantity, :food_id)
   end
 end
