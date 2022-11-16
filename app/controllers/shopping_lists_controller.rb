b1c3a17d47545fa47f62f2e5551c3f1b1f158130
Michael <madetunji1376@gmail.com>
2022-11-16 16:10:48 +0100

    shopping list controller created

---
+++ app/controllers/shopping_lists_controller.rb
@@ -0,0 +1,10 @@
+class ShoppingListsController < ApplicationController
+  def index
+    @user = current_user
+    @foods = @user.foods.all
+    @recipes = @user.recipes.all
+    @list = []
+  end
+
+
+end
