f7a1e11da73ea4ecaa751978d35a7562e915921f
Michael <madetunji1376@gmail.com>
2022-11-16 10:46:21 +0100

    worked on public recipe controller

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,9 @@
 class PublicRecipesController < ApplicationController
   def index
-    
+    @user = current_user
+    @recipes = Recipe.all
+    # @recipe_foods = @recipe.recipe_foods.all
   end
+
+
 end
