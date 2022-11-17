cc29e91c77dd00c03a18a7464d7c8321e3f68f8c
gak92 <44802922+gak92@users.noreply.github.com>
2022-11-17 17:41:40 +0500

    Refactor code in shopping list controller from the view file

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -4,5 +4,24 @@ class ShoppingListsController < ApplicationController
     @foods = @user.foods.all
     @recipes = @user.recipes.all
     @list = []
+
+    @recipes.each do |recipe|
+      recipe.recipe_foods.includes([:food]).all.each do |item|
+        difference = item.quantity - item.food.quantity
+        if difference > 0
+          @list.push([item.food.name, difference, item.food.price * difference])
+        end
+      end
+    end
+
+    @total_amount = 0
+    @list.each do |item|
+      @total_amount += item[2]
+    end
+
+
   end
+
+  
+
 end
